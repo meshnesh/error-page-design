d1bb9837a3c8a2b940ff08e1e62b4eb0a0e3637a
meshnesh <tonnie.nesh@gmail.com>
2017-03-06 14:19:50 +0300

    new onload time duration

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-$(document).foundation()
+// HILL ANIMATION
+$(function(){
+
+    setTimeout(function(){
+        $(".hills").show();
+    }, 100);
+
+});
+// CLOUDS ANIMATION
+$(function(){
+
+    setTimeout(function(){
+        $(".hill1Parent").show();
+    }, 2000);
+
+});
+// 2nd cloud
+$(function(){
+
+    setTimeout(function(){
+        $(".hill2Parent").show();
+    }, 2000);
+
+});
+// TEXT ANIMATION DURATION
+$(function(){
+
+    setTimeout(function(){
+        $(".textParent").show();
+    }, 3000);
+
+});
